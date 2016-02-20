2e91dd266344979f349147d3dbe55761e218600c
Adrián Mouly <adrian@mouly.com.ar>
2016-02-19 23:38:34 -0300

    Added Android project files

---
+++ app/src/main/java/com/preguardia/app/MainActivity.java
@@ -0,0 +1,83 @@
+package com.preguardia.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.batch.android.Batch;
+import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.main_text);
+
+        Firebase myFirebaseRef = new Firebase(BuildConfig.FIREBASE_API_URL);
+
+        myFirebaseRef.child("consultations").setValue("Test value");
+
+        myFirebaseRef.child("consultations").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                textView.setText(snapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(FirebaseError error) {
+            }
+        });
+
+
+        myFirebaseRef.authWithPassword("adrian@mouly.io", "", new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+
+                Batch.User.getEditor()
+                        .setIdentifier(authData.getUid())
+                        .save();
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                // there was an error
+            }
+        });
+    }
+
+    protected void onStart() {
+        super.onStart();
+
+        Batch.onStart(this);
+    }
+
+    @Override
+    protected void onStop() {
+        Batch.onStop(this);
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Batch.onDestroy(this);
+
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Batch.onNewIntent(this, intent);
+
+        super.onNewIntent(intent);
+    }
+}
