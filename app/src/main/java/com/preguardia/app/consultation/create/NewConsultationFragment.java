4a9ba50c524b144b21f9e7fafabcaaab0b21d3ab
Adrián Mouly <adrian@mouly.com.ar>
2016-02-27 21:20:28 -0300

    Created design for New Consultation.

--- app/src/main/java/com/preguardia/app/consultation/create/NewConsultationFragment.java
+++ app/src/main/java/com/preguardia/app/consultation/create/NewConsultationFragment.java
@@ -1,7 +1,16 @@
 package com.preguardia.app.consultation.create;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.preguardia.app.R;
+
+import butterknife.ButterKnife;
 
 /**
  * @author amouly on 2/20/16.
@@ -19,4 +28,19 @@ public class NewConsultationFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_consultation_new, container, false);
+
+        ButterKnife.bind(this, view);
+
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
