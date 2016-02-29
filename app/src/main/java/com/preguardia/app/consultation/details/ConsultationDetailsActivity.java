dbb38d53fbcc55073d67611130159b78f5b9b9be
Adrián Mouly <adrian@mouly.com.ar>
2016-02-28 22:26:14 -0300

    Created Consultation details section.

--- app/src/main/java/com/preguardia/app/consultation/details/ConsultationDetailsActivity.java
+++ app/src/main/java/com/preguardia/app/consultation/details/ConsultationDetailsActivity.java
@@ -3,6 +3,10 @@ package com.preguardia.app.consultation.details;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.preguardia.app.R;
 
@@ -16,6 +20,8 @@ public class ConsultationDetailsActivity extends AppCompatActivity {
 
     @Bind(R.id.consultation_details_toolbar)
     Toolbar toolbar;
+    @Bind(R.id.consultation_details_list)
+    ListView detailsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +30,38 @@ public class ConsultationDetailsActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.consultation_details_title);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.item_details_list, R.id.details_list_text);
+
+        adapter.add("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.");
+        adapter.add("Otra consulta");
+        adapter.add("Otra consulta");
+        adapter.add("Otra consulta");
+        adapter.add("Otra consulta");
+        adapter.add("Otra consulta");
+        adapter.add("Otra consulta");
+
+        detailsList.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_consultation_details, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return true;
     }
 
     @Override
---
+++ app/src/main/java/com/preguardia/app/consultation/details/DetailsListAdapter.java
@@ -0,0 +1,7 @@
+package com.preguardia.app.consultation.details;
+
+/**
+ * @author amouly on 2/28/16.
+ */
+public class DetailsListAdapter {
+}
